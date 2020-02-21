8c678c49557688350db8516f32272443fb0d21b8
Adrian Ferreres <adrian.ferreres@sngular.com>
2020-02-21 16:46:33 +0100

    Add library files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+     name='entity_decorator',  
+     version='1.0.0',
+     scripts=[] ,
+     author="Adrian Ferreres",
+     author_email="ardiadrianadri@gmail.com",
+     description="Decorator for serialize objects in json files",
+     url="https://github.com/practice-microservices-gcp/lib-entity",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
